36653fab2b8345353b2f4e9c8d10e69f4c7a4f65
Andrew Lord <andrew.lord@nuggets.life>
2025-06-03 11:07:03 +0100

    feat: Add Anthropic API integration

--- apps/client/src/config.ts
+++ apps/client/src/config.ts
@@ -1,13 +1,21 @@
 const {
+  CLIENT_TYPE,
   BEDROCK_MODEL_ID,
   BEDROCK_REGION,
+  ANTHROPIC_MODEL_ID,
+  ANTHROPIC_API_KEY,
   NUGGETS_WEBHOOK_API_TOKEN
 } = process.env
 
 const config = {
+  clientType: CLIENT_TYPE,
   bedrock: {
     modelId: BEDROCK_MODEL_ID,
     region: BEDROCK_REGION,
+  },
+  anthropic: {
+    modelId: ANTHROPIC_MODEL_ID,
+    apiKey: ANTHROPIC_API_KEY
   },
   communicator: {
     callbackToken: NUGGETS_WEBHOOK_API_TOKEN
