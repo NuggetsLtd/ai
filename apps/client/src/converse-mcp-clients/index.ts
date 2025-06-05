37740cc7e22a38caf3bb0906013c07965d9b82e4
Andrew Lord <andrew.lord@nuggets.life>
2025-06-05 11:51:51 +0100

    feat: Add Gemini MCP client to demos

--- apps/client/src/converse-mcp-clients/index.ts
+++ apps/client/src/converse-mcp-clients/index.ts
@@ -2,15 +2,17 @@ import type { Message as BedrockMessage } from "@aws-sdk/client-bedrock-runtime"
 import type { MessageParam as AnthropicMessage } from "@anthropic-ai/sdk/resources";
 import { ConverseMcpAnthropicClient } from './anthropic.js'
 import { ConverseMcpBedrockClient } from './aws-bedrock.js'
+import { ConverseMcpGeminiClient } from './gemini.js'
 import { ConverseMcpOpenAiClient } from './openai.js'
 
 enum ClientType {
   AwsBedrock,
   Anthropic,
+  Gemini,
   OpenAI
 }
 
-type ConverseMcpClient = ConverseMcpAnthropicClient | ConverseMcpBedrockClient | ConverseMcpOpenAiClient
+type ConverseMcpClient = ConverseMcpAnthropicClient | ConverseMcpBedrockClient | ConverseMcpGeminiClient | ConverseMcpOpenAiClient
 
 export {
   BedrockMessage,
@@ -19,11 +21,13 @@ export {
   ConverseMcpClient,
   ConverseMcpAnthropicClient,
   ConverseMcpBedrockClient,
+  ConverseMcpGeminiClient,
   ConverseMcpOpenAiClient
 }
 
 export default {
   ConverseMcpAnthropicClient,
   ConverseMcpBedrockClient,
+  ConverseMcpGeminiClient,
   ConverseMcpOpenAiClient
 }
